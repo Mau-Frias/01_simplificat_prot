4634d5ca5cb70cf4282df05c6471a483d9865b5b
Mauricio Frías <mauricio.fr.or@gmail.com>
2023-07-21 14:33:10 -0400

    Nn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -35,29 +35,16 @@ function Navbar({ scrollPos }) {
           <h1>Simplifíca-T</h1>
         </a>
         <button className='nb-hamburger' onClick={handleClick}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-5 w-5'
-            viewBox='0 0 20 20'
-            fill='white'>
-            <path
-              fillRule='evenodd'
-              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z'
-              clipRule='evenodd'
-            />
-          </svg>
+          <img src='/hamburger.svg' />
         </button>
 
         <ul className={`nb-menu ${isExpanded ? 'nb-expanded' : ''}`}>
           {links.map(({ route, name, iconClass }) => (
-            <>
               <li key={name}>
                 <NavLink to={route} className={'nb-link'}>
                   <i className={iconClass} /> {name}
                 </NavLink>
               </li>
-              <hr className='nb-separator' />
-            </>
           ))}
         </ul>
       </div>
